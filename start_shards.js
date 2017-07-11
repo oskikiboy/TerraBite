14888df00baa3b5b619f150d3dbd285323103e27
oskikiboy <oscardavies1@gmail.com>
2017-07-11 19:18:42 +1000

    Test?

--- start_shards.js
+++ start_shards.js
@@ -1,6 +1,12 @@
-const Discord = require('discord.js');
+/*const Discord = require('discord.js');
 const config = require('./config.json');
 const Manager = new Discord.ShardingManager('./app.js', {
   token: config.token
 });
-Manager.spawn();
+Manager.spawn();*/
+
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager(`${__dirname}/app.js`, { totalShards: 3 });
+
+manager.spawn();
+manager.on('launch', shard => console.log(`Successfully launched shard ${shard.id}`));
